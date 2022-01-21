f8a065a903acfdc818a77a37ca4a5c08ccf462fb
Usmonxon Latipov <latipovusmonkhon2006@gmail.com>
2022-01-21 15:28:55 +0500

    update our menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,18 +13,21 @@ class Directory extends React.Component {
                 {
                     title: "hats",
                     imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+                    size: "medium",
                     id: 1,
-                    linkUrl: "shop/hats",
+                    linkUrl: "hats",
                 },
                 {
                     title: "jackets",
                     imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+                    size: "medium",
                     id: 2,
                     linkUrl: "shop/jackets",
                 },
                 {
                     title: "sneakers",
                     imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+                    size: "medium",
                     id: 3,
                     linkUrl: "shop/sneakers",
                 },
@@ -49,13 +52,8 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem
-                        key={id}
-                        title={title}
-                        imageUrl={imageUrl}
-                        size={size}
-                    />
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div
-            className="backgound-image"
-            style={{
-                backgroundImage: `url(${imageUrl})`,
-            }}
-        />
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-);
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
+    console.log(match);
 
-export default MenuItem;
+    return (
+        <div
+            className={`${size} menu-item`}
+            onClick={() => history.push(`${match.url}${linkUrl}`)}
+        >
+            <div
+                className="background-image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+            />
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    );
+};
+
+export default withRouter(MenuItem);
